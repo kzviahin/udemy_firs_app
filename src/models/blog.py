7c4fd3b709c00965bee542d94a2b105cacd72d91
Zvyagintsev-kir <zvyagintsev.kirill@gmail.com>
2016-10-12 22:06:10 +0300

    change blogs, add user model

--- src/models/blog.py
+++ src/models/blog.py
@@ -9,7 +9,29 @@ class Blog(object):
         self.author = author
         self.title = title
         self.description = description
-        self.id = uuid.uuid4().hex if id is None else id
+        self._id = uuid.uuid4().hex if _id is None else _id
 
-    def new_post():
-        pass
+    def new_post(self, title, content, date=datetime.datetime.utcnow()):
+        post = Post(blog_id=self._id,
+                    title=title,
+                    content=content,
+                    author=self.author,
+                    created_date=date)
+        post.save_to_mongo()
+
+    def get_posts(self):
+        return Post.from_blog(self._id)
+
+    def json(self):
+        return {
+            'author': self.author,
+            'title': self.title,
+            'description': self.description,
+            '_id': self._id
+        }
+
+    @classmethod
+    def from_mongo(cls, id):
+        blog_data = Database.find_one(collection='blogs',
+                                      query={'_id': id})
+        return cls(**blog_data)
